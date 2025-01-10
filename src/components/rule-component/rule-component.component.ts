8505894f5b89df5767fca0f7705dc49bda9d0d99
Jon-Angelo <jon.angelo@vlsservices.com>
2025-01-10 15:05:29 +0400

    change in css button design

--- src/components/rule-component/rule-component.component.ts
+++ src/components/rule-component/rule-component.component.ts
@@ -5,25 +5,30 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rule-component',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './rule-component.component.html',
-  styleUrl: './rule-component.component.scss'
+  styleUrl: './rule-component.component.scss',
 })
 export class RuleComponentComponent implements OnInit {
   @Input() condition!: Conditions; // Input property
 
-  ngOnInit(): void {
-  
+  ngOnInit(): void {}
+
+  // Type guard to check if a rule is of type Rule
+  isRule(item: Rule | NestedCondition): item is Rule {
+    return (item as Rule).field !== undefined;
   }
 
-    // Type guard to check if a rule is of type Rule
-    isRule(item: Rule | NestedCondition): item is Rule {
-      return (item as Rule).field !== undefined;
-    }
-  
-    // Type guard to check if a rule is of type NestedCondition
-    isNestedCondition(item: Rule | NestedCondition): item is NestedCondition {
-      return (item as NestedCondition).condition !== undefined;
-    }
+  // Type guard to check if a rule is of type NestedCondition
+  isNestedCondition(item: Rule | NestedCondition): item is NestedCondition {
+    return (item as NestedCondition).condition !== undefined;
+  }
 
+  getButtonClass(value: string): string {
+    if (value.toLowerCase() === 'and' || value.toLowerCase() === 'or') {
+      return 'andOr-button'; // Apply special style for "and" or "or"
+    }
+    return ''; // Default styling for other buttons
+  }
 }
